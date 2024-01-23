ff4d61f9014b8902927a41d9d7a20de4819a79ee
Jakub Sekowski <sekowskijakub@gmail.com>
2024-01-23 09:04:50 +0100

    Init of the project

---
+++ src/test/java/mate/academy/springbookapp/SpringBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.springbookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

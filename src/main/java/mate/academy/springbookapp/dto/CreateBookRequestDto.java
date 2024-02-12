330b887cbf65a3714c2cb6a0a601db9396c4f34f
Jakub Sekowski <sekowskijakub@gmail.com>
2024-02-12 08:52:20 +0100

    Added NotBlank and PositiveOrZero annotations in Dto, fixed pom.xml

--- src/main/java/mate/academy/springbookapp/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/springbookapp/dto/CreateBookRequestDto.java
@@ -1,20 +1,21 @@
 package mate.academy.springbookapp.dto;
 
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank(message = "Title may not be blank")
     private String title;
-    @NotNull
+    @NotBlank(message = "Author may not be blank")
     private String author;
-    @NotNull
+    @NotBlank(message = "ISBN may not be blank")
     private String isbn;
     @NotNull
-    @Min(0)
+    @PositiveOrZero
     private BigDecimal price;
     private String description;
     private String coverImage;

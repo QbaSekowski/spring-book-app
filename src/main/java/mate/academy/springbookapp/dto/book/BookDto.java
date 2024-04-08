825fe555c64dc01c3daede56689eb56b3972af65
Jakub Sekowski <sekowskijakub@gmail.com>
2024-04-08 09:07:53 +0200

    @Data in BookDto replaced with @Getter @Setter and @NoArgsConstructor

--- src/main/java/mate/academy/springbookapp/dto/book/BookDto.java
+++ src/main/java/mate/academy/springbookapp/dto/book/BookDto.java
@@ -2,9 +2,13 @@ package mate.academy.springbookapp.dto.book;
 
 import java.math.BigDecimal;
 import java.util.Set;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class BookDto {
     private Long id;
     private String title;

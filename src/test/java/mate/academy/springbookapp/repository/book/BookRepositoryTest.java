25d8c4b50be8f21918cac1819c6c1b0a7a674411
Jakub Sekowski <sekowskijakub@gmail.com>
2024-04-11 16:37:16 +0200

    First test added in BookRepositoryTest

---
+++ src/test/java/mate/academy/springbookapp/repository/book/BookRepositoryTest.java
@@ -0,0 +1,35 @@
+package mate.academy.springbookapp.repository.book;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import mate.academy.springbookapp.model.Book;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.jdbc.Sql;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BookRepositoryTest {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    @DisplayName("Find all books by valid category ID")
+    @Sql(scripts = {"classpath:database/books/add-three-books-with-categories.sql"},
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {"classpath:database/books/remove-all-books-with-categories.sql"},
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void findAllByCategoryId_ValidCategoryId_ReturnsTwoBooks() {
+        Long correctCategoryId = 1L;
+        List<Book> actual = bookRepository.findAllByCategoryId(correctCategoryId,
+                        PageRequest.of(0, 10))
+                .stream()
+                .toList();
+        assertEquals(2, actual.size());
+    }
+}
---
+++ src/test/resources/database/books/add-three-books-with-categories.sql
@@ -0,0 +1,13 @@
+INSERT INTO categories (id, name, description, is_deleted) VALUES (1, 'Fantasy', 'Fantasy category', 0);
+INSERT INTO categories (id, name, description, is_deleted) VALUES (2, 'Drama', 'Drama category', 0);
+
+INSERT INTO books (id, title, author, isbn, price, description, cover_image, is_deleted)
+VALUES (1, 'Fantasy Book', 'Fantasy Author 1', '978-83-7506-729-3', 40.00, 'Fantasy Description 1', 'images.com/image.jpg', 0);
+INSERT INTO books (id, title, author, isbn, price, description, cover_image, is_deleted)
+VALUES (2, 'Fantasy Book 2', 'Fantasy Author 2', '978-83-080-8011-5', 45.50, 'Fantasy Description 2', 'images.com/image2.jpg', 0);
+INSERT INTO books (id, title, author, isbn, price, description, cover_image, is_deleted)
+VALUES (3, 'Drama Book', 'Drama Author', '978-83-240-6401-4', 69.50, 'Drama Description', 'images.com/image3.jpg', 0);
+
+INSERT INTO books_categories (book_id, category_id) VALUES (1, 1);
+INSERT INTO books_categories (book_id, category_id) VALUES (2, 1);
+INSERT INTO books_categories (book_id, category_id) VALUES (3, 2);
